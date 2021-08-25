6e8fbcb6e565e0b2ffaef8291fc158b103dbe3ea
dineshkrish16 <dineshkrish16@gmail.com>
2021-08-25 08:15:19 +0530

    mph

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,17 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		System.out.println("TCS");
+		
+	}
+	private void companyAddress() {
+		System.out.println("Chennai");
+
+	}
+	public static void main(String[] args) {
+		CompanyInfo d = new CompanyInfo();
+		d.companyName();
+		d.companyAddress();
+	}
+}
